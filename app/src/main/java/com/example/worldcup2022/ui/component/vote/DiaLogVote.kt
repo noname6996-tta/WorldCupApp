8dc19fee476a96fcdefb59371e3159864a4b7fbb
TruyenDev <phamthetruyen@hotmail.com>
2022-11-16 14:28:05 +0700

    TruyenDev - change ui vote dialog

--- app/src/main/java/com/example/worldcup2022/ui/component/vote/DiaLogVote.kt
+++ app/src/main/java/com/example/worldcup2022/ui/component/vote/DiaLogVote.kt
@@ -6,6 +6,7 @@ import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.Window
 import com.bumptech.glide.Glide
 import com.example.worldcup2022.R
@@ -23,7 +24,7 @@ class DiaLogVote(context: Context, match: Match) : Dialog(context) {
         val bind: LayoutDialogVoteBinding = LayoutDialogVoteBinding.inflate(inflater)
         setContentView(bind.root)
         listener(bind, match)
-        Log.e("TAG", "country "+ Gson().toJson(match) )
+        Log.e("TAG", "country " + Gson().toJson(match))
 
 
     }
@@ -36,5 +37,20 @@ class DiaLogVote(context: Context, match: Match) : Dialog(context) {
         Glide.with(context).load(match.country2?.image)
             .error(R.drawable.logo).placeholder(R.drawable.logo)
             .into(bind.ivFlag2)
+
+
+        bind.btnOctopus.setOnClickListener {
+            var random1 = (0..9).random()
+            var random2 = (0..9).random()
+            bind.ivOctopus1.visibility = View.GONE
+            bind.ivOctopus2.visibility = View.GONE
+            if (random1 > random2) {
+                bind.ivOctopus1.visibility = View.VISIBLE
+            } else if (random2 > random1) {
+                bind.ivOctopus2.visibility = View.VISIBLE
+            }
+            bind.edtScores1.setText(random1.toString())
+            bind.edtScores2.setText(random2.toString())
+        }
     }
 }
